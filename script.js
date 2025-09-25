975b66a096e0e3f9d903cc2a7725741f1f5efa09
Tarun <thakurtarun1012@gmail.com>
2025-09-25 20:17:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstWord(s) {
   // your code here
+	let ans = "";
+	for(let i = 0; i < s.length; i++){
+		if(s[i] == " ") return ans;
+		ans += s[i];
+	}
+	
 }
 
 // Do not change the code below
